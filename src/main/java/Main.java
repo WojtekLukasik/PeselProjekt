8cfa3117ddbbdc848c1bf7c070490843c377943d
Wojciech Łukasik <wojtek.lukasik@yahoo.pl>
2019-10-29 22:57:24 +0100

    lols2

---
+++ src/main/java/Main.java
@@ -0,0 +1,32 @@
+import pesel.PESEL;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+
+
+
+
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("Hello! Please type your PESEL number: ");
+//        long firstPeselNumber = input.nextLong();
+//        PESEL first = new PESEL(firstPeselNumber);
+//        if(first.getLen() == 11){
+//            if(first.isValid(first.getDigits())){
+//                System.out.println("Poprawna cyfra kontrolna.");
+//            } else{
+//                System.out.println("Niepoprawna cyfra kontrolna.");
+//            }
+//
+//        } else if (first.getLen() > 11){
+//            System.out.println("PESEL zbyt długi. Podaj poprawny pesel");
+//        } else{
+//            System.out.println("Pesel zbyt krótki. Podaj poprawny pesel.");
+//        }
+//
+//
+//        System.out.println("Your PESEL number is: " + first.getPESEL());
+
+    }
+}
--- src/main/java/pesel/PESEL.java
+++ src/main/java/pesel/PESEL.java
@@ -2,14 +2,42 @@ package pesel;
 
 public class PESEL {
 
-    private int peselNumber;
+    private long peselNumber;
 
-    public PESEL(int peselNumber){
+    public PESEL(long peselNumber){
         this.peselNumber = peselNumber;
     }
 
-    public int getPESEL(){
+    public long getPESEL(){
         return peselNumber;
     }
 
+    public int[] getDigits(){
+        int[] digits = new int[12];
+        int i=10;
+        long peselNumber = this.peselNumber;
+        while(peselNumber > 0){
+            digits[i] = Math.toIntExact(peselNumber % 10);
+            peselNumber = peselNumber / 10;
+            i--;
+        }
+        return digits;
+    }
+
+    public int getLen(){
+        int length = 0;
+        long check = this.peselNumber;
+        while(check > 0){
+            check = check / 10;
+            length++;
+        }
+        return length;
+    }
+
+    public boolean isValid(int[] digits){
+         int a = digits[0], b = digits[1], c = digits[2], d = digits[3], e = digits[4], f = digits[5], g = digits[6],
+                 h = digits[7], i = digits[8], j = digits[9], controlDigit = digits[10];
+         int number = (9 * a + 7 * b + 3 * c + d + 9 * e + 7 * f + 3 * g + h + 9 * i + 7 * j) % 10;
+        return number == controlDigit;
+    }
 }
